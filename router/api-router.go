d5c4b2b909732c355a4061df9c7c42a4dd1cdde7
zhumengstar <1378484330@qq.com>
2026-09-20 13:23:12 +0800

    feat(log): 为 generated-images 路由补充 HEAD 方法支持

--- router/api-router.go
+++ router/api-router.go
@@ -321,6 +321,7 @@ func SetApiRouter(router *gin.Engine) {
 		logRoute.GET("/self", middleware.UserAuth(), controller.GetUserLogs)
 		logRoute.GET("/self/search", middleware.UserAuth(), middleware.SearchRateLimit(), controller.SearchUserLogs)
 		logRoute.GET("/generated-images/:date/:filename", middleware.TryUserAuth(), controller.GetGeneratedImageAsset)
+		logRoute.HEAD("/generated-images/:date/:filename", middleware.TryUserAuth(), controller.GetGeneratedImageAsset)
 
 		systemTaskRoute := apiRouter.Group("/system-task")
 		systemTaskRoute.Use(middleware.RootAuth())
